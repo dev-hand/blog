b0056af72fad8755b0c3ce9886208d9bbe94ad73
devshon <wjdgns200297@gmail.com>
2023-01-12 18:28:26 +0900

    Update

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { GetStaticProps, NextPage } from 'next'
 import { Preview } from 'components/common/editor'
 import { isDev, prefix } from 'infra/config'
 
 const Posts: NextPage<{ data: string }> = ({ data }) => {
-  console.log(data)
+  useEffect(() => {
+    // eslint-disable-next-line no-debugger
+    debugger
+    fetch(`${prefix}/posts/${decodeURI('2021년 회고')}${isDev ? '.md' : ''}`)
+      .then((res) => {
+        console.log(res)
+        return res.text()
+      })
+      .then((text) => text)
+  }, [])
   return <Preview source={data} />
 }
 
@@ -12,11 +21,7 @@ export const getStaticProps: GetStaticProps = async () => {
   const data = await fetch(
     `${prefix}/posts/${decodeURI('2021년 회고')}${isDev ? '.md' : ''}`,
   )
-    .then((res) => {
-      // eslint-disable-next-line no-debugger
-      debugger
-      return res.text()
-    })
+    .then((res) => res.text())
     .then((text) => text)
   return {
     props: { data },
