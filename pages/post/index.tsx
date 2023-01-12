d73ed113d886cbf1ff24e9a2d6eb2cb5366a1f93
devshon <wjdgns200297@gmail.com>
2023-01-12 18:09:15 +0900

    Update

--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,28 +1,28 @@
-// import React from 'react'
-// import { GetServerSideProps, NextPage } from 'next'
-// import { Preview } from 'components/common/editor'
-
-// const Posts: NextPage<{ data: any }> = ({ data }) => {
-//   console.log(data)
-//   return <Preview source={data} />
-// }
-
-// export const getServerSideProps: GetServerSideProps = async () => {
-//   const data = await fetch('http://localhost:3000/posts/2021년 회고.md')
-//     .then((res) => res.text())
-//     .then((text) => text)
-//   return {
-//     props: { data },
-//   }
-// }
-
-// export default Posts
-
-import { NextPage } from 'next'
 import React from 'react'
+import { GetStaticProps, NextPage } from 'next'
+import { Preview } from 'components/common/editor'
 
-const Posts: NextPage = () => {
-  return <div>Posts</div>
+const Posts: NextPage<{ data: any }> = ({ data }) => {
+  console.log(data)
+  return <Preview source={data} />
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await fetch('http://localhost:3000/posts/2021년 회고.md')
+    .then((res) => res.text())
+    .then((text) => text)
+  return {
+    props: { data },
+  }
 }
 
 export default Posts
+
+// import { NextPage } from 'next'
+// import React from 'react'
+
+// const Posts: NextPage = () => {
+//   return <div>Posts</div>
+// }
+
+// export default Posts
