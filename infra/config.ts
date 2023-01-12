72e6e379593ab827f1ef9ae772769ed2b2fbb5eb
devshon <wjdgns200297@gmail.com>
2023-01-12 18:17:55 +0900

    Update

--- infra/config.ts
+++ infra/config.ts
@@ -2,3 +2,5 @@ export const prefix =
   process.env.NODE_ENV === 'production'
     ? 'https://devshon.github.io/blog'
     : 'http://localhost:3000'
+
+export const isDev = process.env.NODE_ENV === 'development'
--- pages/post/index.tsx
+++ pages/post/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { GetStaticProps, NextPage } from 'next'
 import { Preview } from 'components/common/editor'
-import { prefix } from 'infra/config'
+import { isDev, prefix } from 'infra/config'
 
 const Posts: NextPage<{ data: string }> = ({ data }) => {
+  console.log(data)
   return <Preview source={data} />
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data = await fetch(`${prefix}/posts/${decodeURI('2021년 회고')}`)
+  const data = await fetch(
+    `${prefix}/posts/${decodeURI('2021년 회고')}${isDev ? '.md' : ''}`,
+  )
     .then((res) => res.text())
     .then((text) => text)
   return {
