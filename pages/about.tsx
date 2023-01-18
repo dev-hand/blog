0ada41e196ee764eb9f467ba32cedef7d12a1e0c
devshon <wjdgns200297@gmail.com>
2023-01-18 20:50:40 +0900

    Update

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,34 +5,23 @@ import { AboutIntro } from 'components/about/about-intro'
 import { AboutMySelf } from 'components/about/about-my-self'
 import { prefix } from 'infra/config'
 import { AboutPhotoList } from 'components/about/about-photo-list'
-import { posts } from 'public/posts'
-
-// export const getStaticProps: GetStaticProps = async () => {
-//   const data = await fetch(
-//     decodeURI(`${prefix}/posts/리더란 정원사다.md`),
-//   ).then((res) => res.text())
-
-//   return {
-//     props: { data },
-//   }
-// }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data = await Promise.all(
-    posts.map((post) => {
-      return fetch(decodeURI(`${prefix}/posts/${post}.md`))
-    }),
-  ).then((res) => Promise.all(res.map((res) => res.text())))
+  const data = await new Promise((resolve) => {
+    fetch(decodeURI(`${prefix}/posts/리더란 정원사다.md`)).then((response) =>
+      resolve(response.text()),
+    )
+  })
   return {
     props: { data },
   }
 }
 
-const About: NextPage<{ data: string[] }> = ({ data }) => {
+const About: NextPage<{ data: string }> = ({ data }) => {
   return (
     <Frame>
       <AboutIntro />
-      <AboutMySelf data={data[1]} />
+      <AboutMySelf data={data} />
       <AboutPhotoList />
     </Frame>
   )
