561165b57459ed006d5d616a5cb2bf9363ad8a00
devshon <wjdgns200297@gmail.com>
2023-02-16 18:13:59 +0900

    Update

--- components/misc/MdEditorPreview.tsx
+++ components/misc/MdEditorPreview.tsx
@@ -1,19 +1,16 @@
 import '@uiw/react-md-editor/markdown-editor.css'
 import '@uiw/react-markdown-preview/markdown.css'
 import React from 'react'
-import styled from 'styled-components'
 import MdEditor from '@uiw/react-md-editor'
+import { theme } from 'styles/theme'
 
 const MdEditorPreview: React.FC<{
   source: string
-}> = ({ source }) => <Markdown source={source} />
-
-const Markdown = styled(MdEditor.Markdown)`
-  color: ${(p) => p.theme.color.black};
-  background-color: ${(p) => p.theme.color.white};
-  img {
-    width: 100%;
-  }
-`
+}> = ({ source }) => (
+  <MdEditor.Markdown
+    source={source}
+    style={{ color: theme.color.black, backgroundColor: theme.color.white }}
+  />
+)
 
 export default MdEditorPreview
