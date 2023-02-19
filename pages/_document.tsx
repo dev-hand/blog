58c3b92fc84d576f24eaa0dc3f24754f1057d966
devshon <wjdgns200297@gmail.com>
2023-02-19 21:14:20 +0900

    Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,7 +33,20 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name='description' content='many things' />
+          <meta name='description' content='It contains a lot of things.' />
+          <meta property='og:title' content='Many things' />
+          <meta
+            property='og:description'
+            content='It contains a lot of things.'
+          />
+          <meta
+            property='og:url'
+            content='https://devshon.github.io/blog/about/'
+          />
+          <meta
+            property='og:image'
+            content='https://devshon.github.io/blog/images/KakaoTalk_Photo_2023-02-19-20-21-31.jpeg'
+          />
           <link rel='icon' href={`${prefix}/icons/profile.svg`} />
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
