e5daa25ca5f72a24c5f88e539eb4d00ef2c75185
devshon <wjdgns200297@gmail.com>
2023-02-23 01:08:19 +0900

    Update

--- pages/error.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { NextPage } from 'next'
-import { Frame } from 'components/common/Frame'
-
-const Error: NextPage = () => {
-  return <Frame>Error</Frame>
-}
-
-export default Error
