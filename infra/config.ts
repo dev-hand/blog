7174614b7ad15a5c845fef4ec63000a4a8b392a4
-g <wjdgns200297@gmail.com>
2023-07-05 16:41:56 +0900

    Updates

--- infra/config.ts
+++ infra/config.ts
@@ -1,7 +1,7 @@
 export const PREFIX =
   process.env.NODE_ENV === 'production'
     ? 'https://dev-hand.github.io/blog'
-    : 'http://localhost:3000'
+    : 'http://localhost:3001'
 export const IS_DEV = process.env.NODE_ENV === 'development'
 export const SHOW_AUDIO_KEY = 'SHOW_AUDIO'
 export const AMPLITUDE_API_KEY = 'e9f15102bed33c3afa01f12dce9a93ae'
