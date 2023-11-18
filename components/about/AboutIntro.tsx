ce1a291507b9af89c483c30199d3c4472acd6f7e
-g <wjdgns200297@gmail.com>
2023-11-18 21:27:28 +0900

    Update

--- components/about/AboutIntro.tsx
+++ components/about/AboutIntro.tsx
@@ -29,27 +29,27 @@ export const AboutIntro: React.FC = () => {
             <IntroTitleText>시작부터 끝까지!</IntroTitleText>
             <Column>
               <IntroSubText className='one'>
-                {`지금까지 일하면서 프로젝트 성공을 위한 모든 과정을 함께했습니다.`}
+                {`프로젝트 성공을 위한 모든 과정을 함께했습니다.`}
               </IntroSubText>
               <br />
               <IntroSubText className='two'>
-                {`기획, 사용자의 요구사항을 탐색하고, 문제현상을 정의하여 솔루션을 정합니다.`}
+                {`1. 사용자의 요구사항을 탐색하고, 문제현상을 정의하여 솔루션을 도출합니다.`}
               </IntroSubText>
               <br />
               <IntroSubText className='three'>
-                {`설계, 필요한 도구들을 수집하고 모델의 설계와 개발 공수를 측정하여 타임라인을 구축합니다.`}
+                {`2. 필요한 도구들을 수집하고 모델의 설계와 개발 공수를 측정하여 타임라인을 구축합니다.`}
               </IntroSubText>
               <br />
               <IntroSubText className='four'>
-                {`디자인, 사용자 경험에 집중한 심플한 초기 모델을 디자인합니다.`}
+                {`3. 사용자 경험에 집중한 심플한 초기 모델을 디자인합니다.`}
               </IntroSubText>
               <br />
               <IntroSubText className='five'>
-                {`개발, 애자일 방법을 추구합니다.\n커뮤니케이션과 PR문화를 중요하게 생각합니다.\n일관된 코드를 사용하고, 읽기 쉬운 코드를 짜기위해 고민합니다.`}
+                {`4. 애자일 방법을 추구합니다. 커뮤니케이션과 PR문화를 중요하게 생각합니다. 일관된 코드를 사용하고, 읽기 쉬운 코드를 짜기위해 고민합니다.`}
               </IntroSubText>
               <br />
               <IntroSubText className='six'>
-                {`테스트, 배포 후 사용자 데이터를 측정하여 발견된 문제 현상을 기반으로 기획을 반복합니다.`}
+                {`5. 배포 후 사용자 데이터를 측정하여 발견된 문제 현상을 기반으로 기획을 반복합니다.`}
               </IntroSubText>
             </Column>
           </IntroWrapper>
@@ -83,7 +83,7 @@ const IntroTitleText = styled(H2Text)`
     font-size: ${(p) => p.theme.size.h5};
   }
   color: ${(p) => p.theme.color.white};
-  animation: introTitleTextFadeIn 3s 1 ease;
+  /* animation: introTitleTextFadeIn 3s 1 ease; */
   @keyframes introTitleTextFadeIn {
     0% {
       opacity: 0;
@@ -127,7 +127,7 @@ const IntroSubText = styled(DescText)`
       opacity: 1;
     }
   }
-  &.one {
+  /* &.one {
     animation: FadeInOne 3000ms 1 ease;
   }
   &.two {
@@ -144,7 +144,7 @@ const IntroSubText = styled(DescText)`
   }
   &.six {
     animation: FadeInSix 9500ms 1 ease;
-  }
+  } */
 `
 
 const Main = styled(Row)`
